1c8c3a45b2a20a2bea7aaa521db563b5224656ec
互联网部-前端-邬昭霖 <3434933375@qq.com>
2018-09-15 15:50:03 +0800

    node+mysql+jquery实现登录注册

---
+++ README.md
@@ -0,0 +1,14 @@
+Node+mysql+jquery 实现登录注册
+--
+> **首页**  
+> **登录**
+> **注册**
+
+```
+// 将代码clone到本地
+git clone  https://github.com/itzhaolin/Node-mysql-jquery.git
+npm install
+npm start
+```
+
+访问http://127.0.0.1:3000即可.
---
+++ app.js
@@ -0,0 +1,48 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// 模板引擎
+app.engine('html', require('express-art-template'));
+app.set('view engine', 'html');
+
+// 中间键 解析post请求参数
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+// 托管静态资源
+app.use('/public', express.static('public'));
+app.use('/node_modules', express.static('node_modules'));
+
+// 路由容器
+app.use('/', indexRouter);
+app.use(usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
